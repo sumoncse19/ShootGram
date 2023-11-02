808504802ddeabb59bd0a7a6ea8d7a0fbb8349bd
sumoncse19 <mdsumoncse19@gmail.com>
2023-11-03 02:21:48 +0600

    add sign in and sign up form

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex w-full justify-center items-center">
+            <Outlet />
+          </section>
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
